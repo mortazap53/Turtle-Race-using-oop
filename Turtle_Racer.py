f222c84e565d126c4fe8cd22de23386c91180768
R1stAMOR <mortazap53@gmail.com>
2026-02-01 11:53:33 +0430

    the third change

--- Turtle_Racer.py
+++ Turtle_Racer.py
@@ -4,10 +4,10 @@ position = [-80, -40, 0, 40, 80, 120]
 turtle_color = ["red", "green", "blue", "black", "brown", "orange"]
 
 class Racers:
-    def preparing_fields(self, players_numbers):
-        for number in range(players_numbers):
+    def __init__(self, players_numbers):
+        self.players_numbers = players_numbers
+        for number in range(self.players_numbers):
             new_turtle = Turtle(shape="turtle")
             new_turtle.color(turtle_color[number])
             new_turtle.penup()
             new_turtle.goto(-300, y=position[number])
-            self.turtles.append(new_turtle)
--- game.py
+++ game.py
@@ -1,8 +1,9 @@
+from Turtle_Racer import Racers
+from Player import PlayerNumber
 from turtle import Turtle, Screen
 import random
 
-position = [-80, -40, 0, 40, 80, 120]
-turtle_color = ["red", "green", "blue", "black", "brown", "orange"]
+
 
 class Game:
     def __init__(self):
@@ -13,3 +14,17 @@ class Game:
         self.players = {}
         self.turtles_rating = {}
         self.is_race_on = False
+
+    def start_playing(self):
+        self.turtles.append(Racers(self.players))
+
+
+    def number_of_players(self, players_names):
+        count_of_players = int(
+            self.screen.textinput(title="Number of Players", prompt="How many players do you have?"))
+        for numbers in range(count_of_players):
+            player_name = self.screen.textinput(title=f"{numbers + 1} player's name",
+                                                            prompt="What is your name?").title()
+            bet_color = self.screen.textinput(title="Make your bet",
+                                                          prompt=f"Which turtle your are betting on: {turtle_color[0:count_of_players]}: ").lower()
+            players_names[f"{player_name}"] = bet_color
